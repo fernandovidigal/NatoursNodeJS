61d7c5548d588aa2da16b0baf85a1fe1c5d05f3e
Fernando Vidigal <fernandovidigal@gmail.com>
2019-08-29 19:01:24 +0100

    ESLint & Prettier instalation

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).json({message: "Hello from the server side!", app: 'Natours'});
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
