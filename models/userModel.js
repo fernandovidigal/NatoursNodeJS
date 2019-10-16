7fd51b6efcdfafcc934d440f2837c2426f376b58
Fernando Vidigal <fernandovidigal@gmail.com>
2019-10-16 18:43:46 +0100

    Security issue

--- models/userModel.js
+++ models/userModel.js
@@ -41,7 +41,12 @@ const userSchema = new mongoose.Schema({
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
-  passwordResetExpires: Date
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
 });
 
 userSchema.pre('save', async function(next) {
@@ -54,10 +59,16 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
-userSchema.pre('save', function(next){
+userSchema.pre('save', function(next) {
   if (!this.isModified('password') || this.isNew) return next();
 
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function(next) {
+  // This points to the current query
+  this.find({ active: { $ne: false } });
   next();
 });
 
